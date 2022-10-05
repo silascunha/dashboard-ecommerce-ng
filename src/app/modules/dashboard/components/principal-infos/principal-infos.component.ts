a35abdb42d1f19ddf2aa1604eb29d6e71592fe35
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2022-10-05 00:39:28 -0300

    layout do componente com as informações principais

---
+++ src/app/modules/dashboard/components/principal-infos/principal-infos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-infos',
+  templateUrl: './principal-infos.component.html',
+  styleUrls: ['./principal-infos.component.css']
+})
+export class PrincipalInfosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PrincipalInfosComponent } from './components/principal-infos/principal-infos.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    PrincipalInfosComponent
   ],
   imports: [
     CommonModule,
